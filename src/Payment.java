a93a1abd46e2e28718abab2aa31071d33901f9be
Trent <trweatherman1@catamount.wcu.edu>
2015-04-22 14:32:06 -0400

    new size for the payment screen

--- src/Payment.java
+++ src/Payment.java
@@ -54,7 +54,7 @@ public class Payment extends JFrame implements ActionListener{
         //text = total;
 
         this.setContentPane(pane);
-        this.setSize(1000,1000);
+        this.setPreferredSize(new Dimension(800, 600));
         this.setTitle("Payment Screen");
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
